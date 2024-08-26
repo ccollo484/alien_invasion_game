47bf8b506706a2843e6d02de01c13801aadfe7d2
ccollo484 <chriscollo2001@gmail.com>
2024-08-26 13:31:51 +0100

    defined run_game outside __init__ method

--- main_game.py
+++ main_game.py
@@ -6,7 +6,7 @@ import sys
 import pygame
 
 
-class AlienINvasion:
+class AlienInvasion:
     """overall class to manager game assets and behaviour"""
 
     def __init__(self):
@@ -14,20 +14,21 @@ class AlienINvasion:
         pygame.init()
 
         self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion")
 
-        def run_game(self):
-            """start the main loop for the game."""
-            while True:
-                # Watch for keyboard and mouse events
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        sys.exit()
+    def run_game(self):
+        """start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
-                        # Make the most recently drawn screen visible.
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
 
 
 if __name__ == "__main__":
     # make a game instance, and run the game.
-    ai = AlienINvasion()
+    ai = AlienInvasion()
     ai.run_game()
